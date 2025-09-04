235e61e794824e10dd1d202fd3a77423849efd32
jesus24e <jesusrodriguez65@aragon.unam.mx>
2025-09-04 00:02:53 -0600

    carga de la imagen del diagrama  y el archivo sql

--- diagrama_y_sql/archivo.sql
+++ diagrama_y_sql/archivo.sql
@@ -1,15 +1,28 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema sistemas_info
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `sistemas_info` DEFAULT CHARACTER SET utf8 ;
+USE `sistemas_info` ;
+
 -- Tabla de Personas
 CREATE TABLE personas (
-    id_persona INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL
+    `id_persona` INT(5) NOT NULL,
+    `contrasena` VARCHAR(255) NOT NULL,
+    `usuario` VARCHAR(255) NULL,
+    PRIMARY KEY (`id_persona`)
 );
 
 -- Tabla de Oficios
 CREATE TABLE oficios (
-    id_oficio INT PRIMARY KEY AUTO_INCREMENT,
-    numero_oficio VARCHAR(50) NOT NULL,
-    area VARCHAR(100) NOT NULL,
-    fecha DATE NOT NULL,
-    id_persona INT NOT NULL,
-    FOREIGN KEY (id_persona) REFERENCES personas(id_persona)
+    `num_oficio` INT(5) NOT NULL,
+    `persona` VARCHAR(255) NOT NULL,
+    `area` VARCHAR(255) NOT NULL,
+    `asunto` VARCHAR(255) NOT NULL,
+    `fecha` VARCHAR(255) NOT NULL,
+    `hash` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`num_oficio`)
 );
